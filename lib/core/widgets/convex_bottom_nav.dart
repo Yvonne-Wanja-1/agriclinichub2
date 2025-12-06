a654a26bb045a22d557326be1981ccdc926a7dec
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-12-06 16:41:28 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch branch2
    Your branch is up to date with 'origin/branch2'.
    
    Changes to be committed:
    	modified:   lib/core/widgets/convex_bottom_nav.dart

--- lib/core/widgets/convex_bottom_nav.dart
+++ lib/core/widgets/convex_bottom_nav.dart
@@ -16,65 +16,41 @@ class ConvexBottomNav extends StatefulWidget {
 
 class _ConvexBottomNavState extends State<ConvexBottomNav>
     with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late List<AnimationController> _itemAnimationControllers;
+  late AnimationController _scaleController;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 600),
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 400),
       vsync: this,
     );
-
-    _itemAnimationControllers = List.generate(
-      5,
-      (index) => AnimationController(
-        duration: const Duration(milliseconds: 400),
-        vsync: this,
-      ),
-    );
-
-    _animateToIndex(widget.currentIndex);
   }
 
   @override
   void didUpdateWidget(ConvexBottomNav oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.currentIndex != widget.currentIndex) {
-      _animateToIndex(widget.currentIndex);
-    }
-  }
-
-  void _animateToIndex(int index) {
-    for (int i = 0; i < _itemAnimationControllers.length; i++) {
-      if (i == index) {
-        _itemAnimationControllers[i].forward();
-      } else {
-        _itemAnimationControllers[i].reverse();
-      }
+      _scaleController.forward(from: 0.0);
     }
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
-    for (var controller in _itemAnimationControllers) {
-      controller.dispose();
-    }
+    _scaleController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 90,
+      height: 85,
       decoration: BoxDecoration(
         color: Colors.green.shade400,
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.15),
-            blurRadius: 12,
+            color: Colors.black.withOpacity(0.12),
+            blurRadius: 16,
             offset: const Offset(0, -4),
           ),
         ],
@@ -82,53 +58,78 @@ class _ConvexBottomNavState extends State<ConvexBottomNav>
       child: Stack(
         clipBehavior: Clip.none,
         children: [
+          // Bottom nav items
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildAnimatedNavItem(0, Icons.camera_alt, 'Scan'),
-              _buildAnimatedNavItem(1, Icons.history, 'History'),
-              SizedBox(width: 110), // Space for convex curve
-              _buildAnimatedNavItem(2, Icons.person, 'Profile'),
-              _buildAnimatedNavItem(3, Icons.calendar_month, 'Calendar'),
+              _buildNavItem(0, Icons.camera_alt, 'Scan'),
+              _buildNavItem(1, Icons.history, 'History'),
+              SizedBox(width: 120), // Space for center button
+              _buildNavItem(2, Icons.person, 'Profile'),
+              _buildNavItem(3, Icons.calendar_month, 'Calendar'),
             ],
           ),
+          // Center floating home button
           Positioned(
-            top: -30,
+            top: -32,
             left: MediaQuery.of(context).size.width / 2 - 50,
-            child: ScaleTransition(
-              scale: Tween<double>(begin: 0.8, end: 1.0).animate(
-                CurvedAnimation(
-                  parent: _itemAnimationControllers[4],
-                  curve: Curves.elasticOut,
-                ),
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  widget.onTap(4);
-                },
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 16,
-                        spreadRadius: 4,
+            child: TweenAnimationBuilder<double>(
+              tween: Tween(begin: 0.9, end: 1.0),
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.elasticOut,
+              builder: (context, scale, child) {
+                return Transform.scale(
+                  scale: scale,
+                  child: InkWell(
+                    onTap: () => widget.onTap(4),
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    child: TweenAnimationBuilder<double>(
+                      tween: Tween(
+                        begin: widget.currentIndex == 4 ? 0.0 : 0.0,
+                        end: widget.currentIndex == 4 ? 0.08 : 0.0,
                       ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.home,
-                      color: Colors.green.shade600,
-                      size: 44,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      builder: (context, shadowIntensity, _) {
+                        return Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green.shade600.withOpacity(
+                                  0.3 + shadowIntensity,
+                                ),
+                                blurRadius: 20 + (shadowIntensity * 20),
+                                spreadRadius: 2 + (shadowIntensity * 4),
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: ScaleTransition(
+                              scale: Tween<double>(begin: 1.0, end: 1.15)
+                                  .animate(
+                                    CurvedAnimation(
+                                      parent: _scaleController,
+                                      curve: Curves.elasticOut,
+                                    ),
+                                  ),
+                              child: Icon(
+                                Icons.home,
+                                color: Colors.green.shade600,
+                                size: 40,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
           ),
         ],
@@ -136,47 +137,70 @@ class _ConvexBottomNavState extends State<ConvexBottomNav>
     );
   }
 
-  Widget _buildAnimatedNavItem(int index, IconData icon, String label) {
-    return GestureDetector(
-      onTap: () {
-        widget.onTap(index);
-      },
-      child: ScaleTransition(
-        scale: Tween<double>(begin: 1.0, end: 1.15).animate(
-          CurvedAnimation(
-            parent: _itemAnimationControllers[index],
-            curve: Curves.easeInOutBack,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 56,
-              height: 56,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 8,
+  Widget _buildNavItem(int index, IconData icon, String label) {
+    final isActive = widget.currentIndex == index;
+
+    return InkWell(
+      onTap: () => widget.onTap(index),
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween(begin: 0.0, end: isActive ? 1.0 : 0.0),
+        duration: const Duration(milliseconds: 350),
+        curve: Curves.easeInOut,
+        builder: (context, animValue, child) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Icon inside white circle
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(isActive ? 1.0 : 0.2),
+                  shape: BoxShape.circle,
+                  boxShadow: isActive
+                      ? [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.15),
+                            blurRadius: 12,
+                            spreadRadius: 2,
+                          ),
+                        ]
+                      : [],
+                ),
+                child: Center(
+                  child: ScaleTransition(
+                    scale: Tween<double>(begin: 1.0, end: 1.2).animate(
+                      CurvedAnimation(
+                        parent: _scaleController,
+                        curve: Curves.elasticOut,
+                      ),
+                    ),
+                    child: Icon(
+                      icon,
+                      color: isActive ? Colors.green.shade600 : Colors.white,
+                      size: 22,
+                    ),
                   ),
-                ],
+                ),
               ),
-              child: Center(child: Icon(icon, color: Colors.black87, size: 26)),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              label,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 11,
-                fontWeight: FontWeight.w600,
+              const SizedBox(height: 4),
+              // Label below circle
+              AnimatedDefaultTextStyle(
+                duration: const Duration(milliseconds: 300),
+                style: TextStyle(
+                  color: isActive ? Colors.white : Colors.white70,
+                  fontSize: 11,
+                  fontWeight: isActive ? FontWeight.w700 : FontWeight.w500,
+                ),
+                child: Text(label),
               ),
-            ),
-          ],
-        ),
+            ],
+          );
+        },
       ),
     );
   }
