71fdf06f17afe6482faa29e44a7f60e48e326923
Yvonne Wangui <wanguiyvonne333@gmail.com>
2026-01-08 23:21:27 +0300

    Changes to be committed: 	new file:   lib/core/services/notification_service.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'core/router/app_router.dart';
+import 'core/services/notification_service.dart';
+import 'core/services/local_storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.initialize();
+  await NotificationService.requestPermissions();
+  await LocalStorageService.initialize();
   runApp(const AgriClinicHubApp());
 }
 
